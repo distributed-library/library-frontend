81214b0b9e6b4a353335d8ff9dc644101341a22c
Anil Maurya <anil@joshsoftware.com>
2015-06-19 16:33:56 +0530

    Minor fixes, unload usergroup on groups page

--- app/routes/groups.js
+++ app/routes/groups.js
@@ -12,6 +12,8 @@ export default Ember.Route.extend({
     this.store.find('group').then(function(groups){
       controller.set('groups', groups);
     });
+    // Ugly hack to unload previous loaded usergroup (https://github.com/emberjs/data/issues/962)
+    this.store.unloadAll('usergroup');
     this.store.find('usergroup').then(function(user_groups){
       controller.set('user_groups', user_groups);
     });
